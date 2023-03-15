e89a0fe4d9b03f9c1f3090f6c6d02b02eef35e4f
Daniel Ferreira Barbosa <125709368+DanielFBarbosa@users.noreply.github.com>
2023-03-15 02:10:25 -0300

    destructurings

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,12 @@
+const a = 7
+let b = 3
+b += a
+console.log(b)
+b -= 4
+console.log(b)
+b *= 2
+console.log(b)
+b /= 2
+console.log(b)
+b %= 2
+console.log(b)//Se o numero da variavel for par o resultado sera 0 e se impar 1
